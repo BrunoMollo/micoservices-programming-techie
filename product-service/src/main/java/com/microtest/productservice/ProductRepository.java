4ec7890ca88c6eeb8c5d0b1b6c3b2b701ac0fe3a
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-07-19 18:53:30 -0300

    removed old standalone microservices

--- product-service/src/main/java/com/microtest/productservice/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microtest.productservice;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.microtest.productservice.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
